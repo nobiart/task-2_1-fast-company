3649d212f3064ddf7b3f88b2aa67b5736be02cdd
nobiart <dmitri.besleaga@gmail.com>
2021-11-18 11:23:03 +0200

    Module 2. Forms. Part 2. YUP library.

--- src/app/components/ui/loginForm.jsx
+++ src/app/components/ui/loginForm.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { validator } from '../../utils/validator';
+// import { validator } from '../../utils/validator';
 import TextField from '../common/form/textField';
 import CheckBoxField from '../common/form/checkBoxField';
+import * as yup from 'yup';
 
 const LoginForm = () => {
-    const [data, setData] = useState({ email: '', password: '', stayOn: false });
+    const [data, setData] = useState({
+        email: '',
+        password: '',
+        stayOn: false
+    });
     const [errors, setErrors] = useState({});
     const handleChange = (target) => {
         setData((prevState) => ({
@@ -12,37 +17,57 @@ const LoginForm = () => {
             [target.name]: target.value
         }));
     };
-    const validatorConfig = {
-        email: {
-            isRequired: {
-                message: 'Email is required'
-            },
-            isEmail: {
-                message: 'This field requires an email value'
-            }
-        },
-        password: {
-            isRequired: {
-                message: 'Password is required'
-            },
-            isCapitalSymbol: {
-                message: 'Missing capital symbol'
-            },
-            isContainDigit: {
-                message: 'Missing required digit'
-            },
-            min: {
-                message: 'Minimum 8 chars required',
-                value: 8
-            }
-        }
-    };
+    const validateScheme = yup.object().shape({
+        password: yup
+            .string()
+            .required('Password is required')
+            .matches(/(?=.*[A-Z])/, 'Missing capital symbol')
+            .matches(/(?=.*[0-9])/, 'Missing required digit')
+            .matches(
+                /(?=.*[!@#$%^&*])/,
+                'Missing required special character !@#$%^&*'
+            )
+            .matches(/(?=.{8,})/, 'Minimum 8 chars required'),
+        email: yup
+            .string()
+            .required('Email is required')
+            .email('This field requires an email value')
+    });
+    // const validatorConfig = {
+    //     email: {
+    //         isRequired: {
+    //             message: 'Email is required'
+    //         },
+    //         isEmail: {
+    //             message: 'This field requires an email value'
+    //         }
+    //     },
+    //     password: {
+    //         isRequired: {
+    //             message: 'Password is required'
+    //         },
+    //         isCapitalSymbol: {
+    //             message: 'Missing capital symbol'
+    //         },
+    //         isContainDigit: {
+    //             message: 'Missing required digit'
+    //         },
+    //         min: {
+    //             message: 'Minimum 8 chars required',
+    //             value: 8
+    //         }
+    //     }
+    // };
     useEffect(() => {
         validate();
     }, [data]);
     const validate = () => {
-        const errors = validator(data, validatorConfig);
-        setErrors(errors);
+        // const errors = validator(data, validatorConfig);
+        validateScheme
+            .validate(data)
+            .then(() => setErrors({}))
+            .catch((err) => setErrors({ [err.path]: err.message }));
+        // setErrors(errors);
         return Object.keys(errors).length === 0;
     };
     const isValid = Object.keys(errors).length === 0;
