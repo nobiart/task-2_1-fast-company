cabb52c47f8c14c43b787a2d101d4bb5d61e0aab
nobiart <dmitri.besleaga@gmail.com>
2021-11-27 01:40:30 +0200

    Module 2. Forms. Part 2. Add userInfoCard

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
 import API from '../../../api';
 import Qualities from '../../ui/qualities';
 import SelectField from '../../common/form/selectField';
+import UserInfoCard from '../../ui/cards/userInfoCard';
 
 const UserPage = ({ userId }) => {
-    const history = useHistory();
     const [data, setData] = useState({
         _id: '',
         userId: '',
@@ -20,9 +19,6 @@ const UserPage = ({ userId }) => {
         API.users.getById(userId).then((data) => setUser(data));
         API.users.fetchAll().then((data) => setUsers(data));
     }, []);
-    const handleClick = () => {
-        history.push(`/users/${userId}/edit`);
-    };
     const handleChange = (target) => {
         setData((prevState) => ({
             ...prevState,
@@ -33,66 +29,7 @@ const UserPage = ({ userId }) => {
         return (
             <div className="row gutters-sm">
                 <div className="col-md-4 mb-3">
-                    <div className="card mb-3">
-                        <div className="card-body">
-                            <button
-                                className="
-                                    position-absolute
-                                    top-0
-                                    end-0
-                                    btn btn-light btn-sm
-                                "
-                                onClick={handleClick}
-                            >
-                                <i className="bi bi-gear" />
-                            </button>
-                            <div
-                                className="
-                                    d-flex
-                                    flex-column
-                                    align-items-center
-                                    text-center
-                                    position-relative
-                                "
-                            >
-                                <img
-                                    src={`https://avatars.dicebear.com/api/avataaars/${(
-                                        Math.random() + 1
-                                    )
-                                        .toString(36)
-                                        .substring(7)}.svg`}
-                                    className="rounded-circle"
-                                    width="150"
-                                    alt={user.name}
-                                />
-                                <div className="mt-3">
-                                    <h4>{user.name}</h4>
-                                    <p className="text-secondary mb-1">
-                                        {user.profession.name}
-                                    </p>
-                                    <div className="text-muted">
-                                        <i
-                                            className="
-                                                bi bi-caret-down-fill
-                                                text-primary
-                                            "
-                                            role="button"
-                                        />
-                                        <i
-                                            className="
-                                                bi bi-caret-up
-                                                text-secondary
-                                            "
-                                            role="button"
-                                        />
-                                        <span className="ms-2">
-                                            {user.rate}
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <UserInfoCard userId={userId} />
                     <div className="card mb-3">
                         <div
                             className="
