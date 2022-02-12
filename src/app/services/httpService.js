da0a270517fb88c25ea5d4a5b02455a67bea08fe
nobiart <dmitri.besleaga@gmail.com>
2022-02-12 21:19:59 +0200

    Module 2. Authorized request

--- src/app/services/httpService.js
+++ src/app/services/httpService.js
@@ -27,6 +27,10 @@ http.interceptors.request.use(
                     localId: data.user_id
                 });
             }
+            const accessToken = localStorageService.getAccessToken();
+            if (accessToken) {
+                config.params = { ...config.params, auth: accessToken };
+            }
         }
         return config;
     },
